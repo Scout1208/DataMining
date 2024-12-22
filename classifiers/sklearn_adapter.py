3b6d6c8abfd7121386b8e3f33bb03b66331b2270
chengyu <yuuuyy5@gmail.com>
2024-12-23 01:36:57 +0800

    add params avaliable

--- classifiers/sklearn_adapter.py
+++ classifiers/sklearn_adapter.py
@@ -2,12 +2,12 @@ from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.ensemble import RandomForestClassifier
 
 class SklearnClassifierAdapter(BaseEstimator, ClassifierMixin):
-    def __init__(self, base_classifier=None):
+    def __init__(self, base_classifier=None, params = None):
         super().__init__()
         if base_classifier is None:
             self.base_classifier = RandomForestClassifier()
         else:
-            self.base_classifier = base_classifier()
+            self.base_classifier = base_classifier(**(params or {}))
     
     def fit(self, X, y):
         self.base_classifier.fit(X, y)
