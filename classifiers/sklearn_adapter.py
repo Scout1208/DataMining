23fed797b59c8151268cb5fa0f73ecb2de427786
chengyu <yuuuyy5@gmail.com>
2024-12-22 16:35:12 +0800

    add sklearn adapter

---
+++ classifiers/sklearn_adapter.py
@@ -0,0 +1,20 @@
+from sklearn.base import BaseEstimator, ClassifierMixin
+from sklearn.ensemble import RandomForestClassifier
+
+class SklearnClassifierAdapter(BaseEstimator, ClassifierMixin):
+    def __init__(self, base_classifier=None):
+        super().__init__()
+        if base_classifier is None:
+            self.base_classifier = RandomForestClassifier()
+        else:
+            self.base_classifier = base_classifier()
+    
+    def fit(self, X, y):
+        self.base_classifier.fit(X, y)
+        return self
+    
+    def predict(self, X):
+        return self.base_classifier.predict(X)
+    
+    def score(self, X, y):
+        return self.base_classifier.score(X, y)
